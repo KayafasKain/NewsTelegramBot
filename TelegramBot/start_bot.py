cdddf67e622b70fdebea11bd212a109e43651934
KayafasKain <bismark9988@gmail.com>
2018-12-01 16:05:53 +0200

    initial bot

--- TelegramBot/start_bot.py
+++
@@ -1,5 +0,0 @@
-from bot_database import DBOperator
-from settings import NewsAggregatorClient
-from settings import TelegramBotToken
-from bot_core import BotCore
-BotCore(job_interval=600, token=TelegramBotToken, db=DBOperator(NewsAggregatorClient))
