8beb8dc04825d3370e0ad48a6027897102cc7416
KayafasKain <bismark9988@gmail.com>
2018-12-01 17:57:43 +0200

    make it better

--- TelegramBot/start_bot.py
+++ TelegramBot/start_bot.py
@@ -4,6 +4,4 @@ from settings import TelegramBotToken
 from bot_core import BotCore
 import socket
 
-sock = socket.socket()
-sock.bind(('', 2288))
 BotCore(job_interval=60, token=TelegramBotToken, db=DBOperator(NewsAggregatorClient))
