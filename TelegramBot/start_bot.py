482b64c1954c1e7b0bbae6dec7b8e427409e6f13
KayafasKain <bismark9988@gmail.com>
2018-12-01 18:06:15 +0200

    make it better

--- TelegramBot/start_bot.py
+++ TelegramBot/start_bot.py
@@ -2,6 +2,14 @@ from bot_database import DBOperator
 from settings import NewsAggregatorClient
 from settings import TelegramBotToken
 from bot_core import BotCore
-import socket
+import os
 
+
+port = int(os.environ.get("PORT", 5000))
+
+
+from flask import Flask
+
+app = Flask(__name__)
 BotCore(job_interval=60, token=TelegramBotToken, db=DBOperator(NewsAggregatorClient))
+app.run(debug=False, host='0.0.0.0', port=port)
